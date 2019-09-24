e102850b02a5eaf1f02ca279622d4fede8584316
solomiia <solomiiagulitashvili@gmail.com>
2019-09-24 23:03:07 +0300

    разметка с модалочками

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "popper.js/dist/popper.min.js";
+import "jquery/dist/jquery.slim.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "../scss/index.scss";
+// $("#exampleModalCenter").modal();
