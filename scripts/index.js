783ad4886dea3bc9b8143ae990acdc3fa0ea797d
solomiia <solomiiagulitashvili@gmail.com>
2019-09-25 22:24:18 +0300

    модалки, котошаблон, гугл кнопка аддед

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,8 @@ import "popper.js/dist/popper.min.js";
 import "jquery/dist/jquery.slim.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "../scss/index.scss";
+
+import "./rendering.js";
+import "./data.js";
+import "./button.js";
 // $("#exampleModalCenter").modal();
